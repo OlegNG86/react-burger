dffd01a7326f9a92105af9298fab79b69a6cea28
Pykhalov Oleg <pykhalov.oleg@gmail.com>
2024-02-25 22:15:12 +0300

    add icons for modal

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,6 +1,6 @@
 import styles from './ingredient-details.module.css';
 import PropTypes from 'prop-types';
-
+import iconClose from '../images/icon-close.svg';
 
 function IngredientDetails({ closeModal, title, ingredientData }) {
     const { image_large, name, calories, carbohydrates, fat, proteins } = ingredientData;
@@ -9,7 +9,7 @@ function IngredientDetails({ closeModal, title, ingredientData }) {
         <div className={`pl-10 pr-10 ${styles.ingredient}`}>
             <div className={`mt-10 ${styles.ingredient__header}`}>
                 <h2 className={`text text_type_main-large ${styles.ingredient__title}`}>{title}</h2>
-                <button onClick={closeModal} type='button' className={`${styles.ingredient__closeButton}`}><img src='{closeButtonImg}' alt='Закрыть окно' /></button>
+                <button onClick={closeModal} type='button' className={`${styles.ingredient__closeButton}`}><img src={iconClose} alt='Закрыть окно' /></button>
             </div>
             <img className={`mt-15 mb-15 ${styles.ingredient__image}`} src={image_large} alt='Ингредиент' />
             <p className={`text text_type_main-medium mt-4 mb-8 ${styles.ingredient__name}`}>{name}</p>
--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
 import styles from './order-details.module.css';
-
+import iconDone from '../images/icon-done.svg';
+import iconClose from '../images/icon-close.svg';
 
 function OrderDetails({ orderId, closeModal }) {
 
     return (
         <div className={styles.order}>
-            <button onClick={closeModal} type='button' className={`${styles.order__closeButton}`}><img src='' alt='Закрыть окно' /></button>
+            <button onClick={closeModal} type='button' className={`${styles.order__closeButton}`}><img src={iconClose} alt='Закрыть окно' /></button>
             <h2 className={`text text_type_digits-large mt-30 mb-8 ${styles.order__title}`}>{orderId}</h2>
             <p className={`text text_type_main-medium ${styles.order__id}`}>Идентификатор заказа</p>
-            <img className={`mt-15 mb-15 ${styles.order__image}`} src='' alt='Ваш заказ принят' />
+            <img className={`mt-15 mb-15 ${styles.order__image}`} src={iconDone} alt='Ваш заказ принят' />
             <p className={`text text_type_main-default ${styles.order__description}`}>Ваш заказ начали готовить</p>
             <p className={`text text_type_main-default mt-2 mb-30 ${styles.order__descriptionReady}`}>Дождитесь готовности на орбитальной станции</p>
         </div>
