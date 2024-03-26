fa53c0cf0312acf84b27644bde3d2b2a4959e2fe
Pykhalov Oleg <pykhalov.oleg@gmail.com>
2024-03-26 23:57:36 +0300

    extract home from app to new component

---
+++ src/components/pages/login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./login.module.css";
+
+function LoginPage() {
+  return (
+    <div className={styles.wrapper}>
+      <form className={styles.form}>
+        <h1 className={styles.heading}>Вход</h1>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ src/components/pages/reset-password.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./order-details.module.css";
+import iconDone from "../../images/icon-done.png";
+import { useDispatch, useSelector } from "react-redux";
+import { setOrderId, setError } from "../../services/actions/order-details";
+
+function ResetPasswordPage() {
+  const dispatch = useDispatch();
+  const { orderId, error } = useSelector((store) => store.order);
+
+  return (
+    <div className={styles.order}>
+      <h2
+        className={`text text_type_digits-large mt-30 mb-8 ${styles.order__title}`}
+      >
+        {orderId}
+      </h2>
+      <p className={`text text_type_main-medium ${styles.order__id}`}>
+        Идентификатор заказа
+      </p>
+      <img
+        className={`mt-15 mb-15 ${styles.order__image}`}
+        src={iconDone}
+        alt="Ваш заказ принят"
+      />
+      <p className={`text text_type_main-default ${styles.order__description}`}>
+        Ваш заказ начали готовить
+      </p>
+      <p
+        className={`text text_type_main-default mt-2 mb-30 ${styles.order__descriptionReady}`}
+      >
+        Дождитесь готовности на орбитальной станции
+      </p>
+    </div>
+  );
+}
+
+export default ResetPasswordPage;
