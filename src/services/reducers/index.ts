dea134ea3d9e62e88b8654969fa9a825391e18ac
Pykhalov Oleg <pykhalov.oleg@gmail.com>
2024-05-01 04:02:58 +0300

    add types for thunk and store

--- src/services/reducers/index.ts
+++ src/services/reducers/index.ts
@@ -6,6 +6,14 @@ import { modalReducer } from "./modal";
 import orderReducer from "./order-details";
 import authorizationReducer from "./authorization";
 import userReducer from "./user";
+import { ThunkAction } from 'redux-thunk';
+import { Action, ActionCreator } from 'redux';
+import { TAuthorizationActions } from "../actions/authorization";
+import { TBurgerConstructorActions } from "../actions/burger-constructor";
+import { TBurgerIngredientsActions } from "../actions/burger-ingredients";
+import { TModalActions } from "../actions/modal";
+import { TOrderDetailsActions } from "../actions/order-details";
+import { TUserActions } from "../actions/user";
 
 export const rootReducer = combineReducers({
   ingredients: burgerIngredientsReducer,
@@ -22,4 +30,21 @@ export const store = configureStore({
 
 // export type RootState = ReturnType<typeof store.getState>; изначальная типизация стора
 export type RootState = ReturnType<typeof rootReducer>; //вариант от наставника
+
+
+// Типизация всех экшенов приложения
+type TApplicationActions =
+  | TAuthorizationActions
+  | TBurgerConstructorActions
+  | TBurgerIngredientsActions
+  | TModalActions
+  | TOrderDetailsActions
+  | TUserActions;
+
+  // Типизация thunk'ов в нашем приложении
+export type AppThunk<TReturn = void> = ActionCreator<
+ThunkAction<TReturn, Action, RootState, TApplicationActions>
+>; 
+
+// Типизация метода dispatch для проверки на валидность отправляемого экшена
 export type AppDispatch = typeof store.dispatch;
